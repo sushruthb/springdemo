b7a077cd886ec48fda21c2096bee105b57dc190e
Sushruth Bharath HG <bharathhg@gmail.com>
2019-11-04 15:03:43 +0530

    Added security

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+/*
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,3 +14,4 @@ public class HelloController {
 
 
 }
+*/
