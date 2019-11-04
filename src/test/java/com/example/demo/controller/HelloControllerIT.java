ac7127666a0e5c056217a0b925c7be0d4b95900a
Sushruth Bharath HG <bharathhg@gmail.com>
2019-11-04 15:15:32 +0530

    Added security

--- src/test/java/com/example/demo/controller/HelloControllerIT.java
+++ src/test/java/com/example/demo/controller/HelloControllerIT.java
@@ -15,8 +15,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerIT {
 
     @LocalServerPort
--- src/test/java/com/example/demo/controller/HelloControllerTest.java
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -15,9 +15,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@AutoConfigureMockMvc
 public class HelloControllerTest {
 
     @Autowired
